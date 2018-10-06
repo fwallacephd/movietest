0954157780c3ba205bfff7f5361694f9ce087746
Faith Wallace <fwallacephd@gmail.com>
2018-10-06 06:59:41 -0400

    Modal Research

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+Modal.setAppElement('#ether')
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+  class ExampleApp extends React.Component {
+    constructor() {
+      super();
+   
+      this.state = {
+        modalIsOpen: false
+      };
+   
+      this.openModal = this.openModal.bind(this);
+      this.afterOpenModal = this.afterOpenModal.bind(this);
+      this.closeModal = this.closeModal.bind(this);
+    }
+   
+    openModal() {
+      this.setState({modalIsOpen: true});
+    }
+   
+    afterOpenModal() {
+      // references are now sync'd and can be accessed.
+      this.subtitle.style.color = '#f00';
+    }
+   
+    closeModal() {
+      this.setState({modalIsOpen: false});
+    }
+   
+    render() {
+      return (
+        <div>
+          <button onClick={this.openModal}>Open Modal</button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+   
+            <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+            <button onClick={this.closeModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+              <input />
+              <button>tab navigation</button>
+              <button>stays</button>
+              <button>inside</button>
+              <button>the modal</button>
+            </form>
+          </Modal>
+        </div>
     );
   }
 }
 
-export default App;
+ReactDOM.render(<App />, appElement);
